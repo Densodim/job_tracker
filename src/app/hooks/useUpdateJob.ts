b48e7b3a19a96bc5842119c0eedfac7489ecf052
Densodim <25denso@gmail.com>
2024-12-05 22:24:41 +0300

    add PUBLIC URL

--- src/app/hooks/useUpdateJob.ts
+++ src/app/hooks/useUpdateJob.ts
@@ -1,5 +1,9 @@
-import { BASE_URL } from "@/app/hooks/useJobsQuery"
 import useJobs from "@/app/hooks/useJobs"
+
+const isProduction = process.env.NODE_ENV === 'production';
+export const BASE_URL = isProduction
+    ? process.env.NEXT_PUBLIC
+    : 'http://localhost:3000';
 
 export function useUpdateJob() {
   const query = useJobs()
