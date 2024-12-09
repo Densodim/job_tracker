9b3b06cff299a9cc6f8541a9e8989a4007e81fb2
Densodim <25denso@gmail.com>
2024-12-10 01:30:09 +0300

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import LoginForm from "@/app/components/LoginForm"
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full">
-      <h1>Для входа введите любой email(например email@email.com) и пароль 6 цифр</h1>
+      <h1>
+        Для входа введите любой email(например email@email.com) и пароль 6 цифр
+      </h1>
       <LoginForm />
     </div>
   )
