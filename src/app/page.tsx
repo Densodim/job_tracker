2610a7ecc417b8061f15a651e673e59f8dd8baf8
Densodim <25denso@gmail.com>
2024-12-09 17:59:23 +0300

    delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 
 
 import React from "react";
-import Login from "@/ui/components/auth/Login/login";
+import {redirect} from "next/navigation";
+
 
 
 export default function Home() {
-  // redirect("/jobs")
+  redirect("/jobs")
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <h1>Page home</h1>
-        <Login/>
+
     </div>
   )
 }
